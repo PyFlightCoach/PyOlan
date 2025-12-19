9abe56ab9af1b94c889e2181e665bccd547db755
Thomas David <thomasdavid0@gmail.com>
2025-12-19 17:02:11 +0000

    bugfix

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,18 +1,21 @@
 import numpy as np
-from pytest import approx
-from schemas.positioning import Direction
+from pytest import approx, fixture
+from schemas.positioning import Direction, Heading
 from tuning.builders import load_builder
-
+from flightanalysis.builders.manbuilder import ManBuilder
 from pyolan.parser import ParsedOlanFig, parse_olan
+from flightdata import State
 
-mb = load_builder("IAC")
+@fixture(scope="session")
+def mb() -> ManBuilder:
+    return load_builder("IAC", category="unlimited")
 
-def test_entry_direction():
+def test_entry_direction(mb: ManBuilder):
     ofigs = parse_olan("13% -``5if```,4ao(,1),22+`", mb)
     assert ofigs[0].aresti.info.start.direction == Direction.UPWIND
 
 
-def test_create_unl_loop_definition():
+def test_create_unl_loop_definition(mb: ManBuilder):
 
     ofig: ParsedOlanFig = parse_olan("`+o34;5f+`", mb)[0]
 
@@ -20,7 +23,7 @@ def test_create_unl_loop_definition():
     #assert "point_length" in ofig.definition.mps.data.keys()
 
 
-def test_imac_sportsman_2025():
+def test_imac_sportsman_2025(mb: ManBuilder):
     oseq = parse_olan(
         "/d'1 5% 2a,f (-3,6) 4h4^> p(2)...' dq 4% 2b.''1.''+``` (-13,0) 3% ~2g~ (2,0) 3% iv```6s.....'' 22y````1.. (-3,0) 8% `24'zt`8''",
         mb,
@@ -28,7 +31,7 @@ def test_imac_sportsman_2025():
     assert len(oseq) == 10
 
 
-def test_imac_inter_2025():
+def test_imac_inter_2025(mb: ManBuilder):
     oseq = parse_olan(
         "d'f 5% 44a,if- (-3,6) -4h2,4-^> -,ify...''22 (-1,0) -2% 1dq1 1j1> 3% ``1,24pb...34..+++ 3% ~`1,48``g~ (5,0) 3% ```9s....'ikz----~~ /-22iy2",
         mb,
@@ -36,7 +39,7 @@ def test_imac_inter_2025():
     assert len(oseq) == 10
 
 
-def test_turn():
+def test_turn(mb: ManBuilder):
     fig: ParsedOlanFig = parse_olan("4j", mb)[0]
 
     assert fig.aresti.elements[0].kind == "roll"
@@ -46,11 +49,13 @@ def test_turn():
     assert fig.definition.eds[2].props["ke"] == approx(np.radians(30))
 
 
-def test_tailslide():
+def test_tailslide(mb: ManBuilder):
     fig: ParsedOlanFig = parse_olan("~~.,3ita'3....~>", mb)
 
 
-def test_iac_unl_2026_n():
+def test_iac_unl_2026_n(mb: ManBuilder):
     fig: ParsedOlanFig = parse_olan("7% ``-if....'in(.,3,34...'').....''-~~", mb)[0]
+    
+
 
     pass
